d7086cd34c1e89a2982c09f4519bae86e8b5790e
shady-cj <petersp2000@gmail.com>
2021-08-24 23:21:10 +0100

    webscraping mojo box office

--- firstscrape.py
+++ firstscrape.py
@@ -1,31 +1,45 @@
 import requests
 from requests_html import HTML
 import pandas as pd
-req=requests.get("https://www.boxofficemojo.com/daily/2021/")
-if req.status_code == 200:
-    parsed_html=HTML(html=req.text)
-    container=parsed_html.find('.imdb-scroll-table')
-    tablecontainer=container[0].find('.imdb-scroll-table-inner')
-    rows= tablecontainer[0].find('tr')
-    mainlist=[]
-    mainheader = rows[0].find('th')
-    header_names = []
-    for headername in mainheader:
-        header_names.append(headername.text)
-
-    mainlist.append(header_names)
-    for row in rows[1:]:
-        cols=row.find('td')
-        
-        sublist=[]
-        for col in cols:
-            sublist.append(col.text)
-
-        mainlist.append(sublist)
-
-    print(mainlist)
-
-df = pd.DataFrame(mainlist, columns = header_names) 
-df.to_csv('movies.csv',index=False)
+import os 
 
 
+BASE_DIR = os.path.dirname(__file__)
+def scrape(startyear=2020,span=1):
+    assert isinstance(startyear, int)
+    assert isinstance(span, int)
+    for s in range(0,span+1):
+        generate(startyear)
+        startyear+=1
+
+def generate(year = 2021):
+    req=requests.get(f"https://www.boxofficemojo.com/daily/{year}/")
+    if req.status_code == 200:
+        parsed_html=HTML(html=req.text)
+        container=parsed_html.find('.imdb-scroll-table')
+        tablecontainer=container[0].find('.imdb-scroll-table-inner')
+        rows= tablecontainer[0].find('tr')
+        mainlist=[]
+        mainheader = rows[0].find('th')
+        header_names = []
+        for headername in mainheader:
+            header_names.append(headername.text)
+
+        for row in rows[1:]:
+            cols=row.find('td')
+            
+            sublist=[]
+            for col in cols:
+                sublist.append(col.text)
+
+            mainlist.append(sublist)
+
+    movie = os.path.join(BASE_DIR,"movies")
+    os.makedirs("movies",exist_ok=True)
+    dirfile=os.path.join(movie,f"{year}.csv")
+    df = pd.DataFrame(mainlist, columns = header_names) 
+    df.to_csv(dirfile,index=False)
+
+
+if __name__ == "__main__":
+    scrape(startyear=2012,span=6)
