b6426aff39193aa71bf8f01df19e14e66c4fd113
shady-cj <petersp2000@gmail.com>
2021-08-24 22:26:51 +0100

    new modification

--- firstscrape.py
+++ firstscrape.py
@@ -1,24 +1,31 @@
 import requests
 from requests_html import HTML
-
+import pandas as pd
 req=requests.get("https://www.boxofficemojo.com/daily/2021/")
 if req.status_code == 200:
     parsed_html=HTML(html=req.text)
     container=parsed_html.find('.imdb-scroll-table')
     tablecontainer=container[0].find('.imdb-scroll-table-inner')
-    innertable = tablecontainer[0].find('tr')
+    rows= tablecontainer[0].find('tr')
     mainlist=[]
-    mainheader = innertable[0].find('th')
-    
-    for i,k in enumerate(innertable[1:]):
-        data=k.find('td')
-        print(i,k)
+    mainheader = rows[0].find('th')
+    header_names = []
+    for headername in mainheader:
+        header_names.append(headername.text)
+
+    mainlist.append(header_names)
+    for row in rows[1:]:
+        cols=row.find('td')
         
-        # for k in data:
-        #     sublist.append(k.text)
+        sublist=[]
+        for col in cols:
+            sublist.append(col.text)
 
-        # print(sublist)
+        mainlist.append(sublist)
 
+    print(mainlist)
 
+df = pd.DataFrame(mainlist, columns = header_names) 
+df.to_csv('movies.csv',index=False)
 
 
